c006afd63c44bc0054de7afb949334c390074403
Rezuan <rezuan@pop-os.localdomain>
2021-06-07 23:49:11 +0600

    final

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-const connectDB = require("./config/db");
+const path = require('path')
+const connectDB = require("./config/db")
 const cors = require('cors')
 
 // route
@@ -11,7 +12,7 @@ connectDB()
 
 // app instance
 const app = express()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8080
 app.use(cors())
 
 // body parsing middleware
@@ -19,6 +20,16 @@ app.use(express.json())
 
 
 app.use('/api/user', userRoutes)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`)
